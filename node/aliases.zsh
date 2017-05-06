2797e75d295e83aa77269d8669613862cc076d6f
Bruno <iam@corrni.io>
2017-05-06 23:27:55 +0200

    yarn helpers

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-alias npis='npm install --save --verbose'
-alias npisd='npm install --save-dev --verbose'
-alias npig='npm install -g --verbose'
-alias npit='npm init --verbose'
-alias npity='npm init -y --verbose'
-alias npi='npm install --verbose'
+alias npis='npm install --save'
+alias npisd='npm install --save-dev'
+alias npig='npm install -g'
+alias npit='npm init'
+alias npity='npm init -y'
+alias npi='npm install'
+
+### Yarn
+#
+alias yarn='f() { yarn $1 --silent };f'
+
+alias yga='yarn global add'
+alias ya='yarn add'
+alias yad='yarn add -D'
+
+# instead of 'npm run'
+alias y='yarn'
+alias yr='yarn run'
+alias yrs='yarn start'
+alias yrt='yarn test'
+
