a3ac5d6cadbdca2af69e18c4c8fe4b8a48e9825e
Bruno Bakundwa <corrni@bingeau.local>
2017-08-20 23:32:27 +0200

    A few more node aliases

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -11,17 +11,17 @@ alias npi='npm install'
 # yarn
 alias yg='yarn global'
 alias ya='yarn add'
-alias yag='yarn global add'
+alias yga='yarn global add'
 alias yad='yarn add -D'
 
 alias yr='yarn run'
 alias yrs='yarn start'
 alias yrt='yarn test'
 alias yrl='yarn run lint'
-# alias yr='f() { yarn run $1 --silent };f'
-# alias yrs='f() { yarn start $1 --silent };f'
-# alias yrt='f() { yarn test $1 --silent };f'
-# alias yrl='f() { yarn run lint $1 --silent };f'
+
+# flow
+alias fts='flow-typed search'
+alias ftd='flow-typed --flowVersion=0.52.0' # more libdefs available for <= 0.52.x
 
 # for chrome headless
 # SEE: https://developers.google.com/web/updates/2017/04/headless-chrome
