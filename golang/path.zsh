f8665f88d1b8b52f458cf40207511298a7f57054
Bruno <iam@corrni.io>
2017-11-23 20:44:08 +0100

    homebrew & golang path, installs, ...

--- golang/path.zsh
+++ golang/path.zsh
@@ -1,21 +1,9 @@
 #!/bin/sh
 
-if [ "$(which go)" ] && [ -n "$GOPATH" ]; then
-	mkdir -p "$GOPATH/bin" "$GOPATH/src/github.com/"
+export GOPATH="$PROJECTS/Go"
+[ ! -d "$GOPATH" ] &&  mkdir -p "$GOPATH/bin"
+[ ! -d "$GOPATH/src/github.com/" ] && mkdir -p "$GOPATH/src/github.com/"
+export PATH="$GOPATH/bin:$PATH"
 
-	packages="
-    github.com/alecthomas/gometalinter
-    golang.org/x/tools/cmd/cover
-    golang.org/x/tools/cmd/godoc
-    github.com/nsf/gocode
-    golang.org/x/tools/cmd/guru
-    golang.org/x/tools/cmd/goimports
-    golang.org/x/tools/cmd/gorename
-    github.com/golang/lint/golint
-  "
-
-	for pkg in $packages; do
-		go get -u "$pkg"
-	done
-	"$GOPATH"/bin/gometalinter --install --update
-fi
+# Homebrew setup recommendation
+export PATH=$PATH:/usr/local/opt/go/libexec/bin
