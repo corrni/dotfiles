6c2e08846947f61636f2585947b45e1688eda7e3
Bruno <iam@corrni.io>
2017-11-23 19:58:49 +0100

    zsh stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,6 @@
 #!/bin/sh
 
-# dotfiles reloading
-__reload_dotfiles() {
-  PATH="$(command -p getconf PATH):/usr/local/bin"
-  # shellcheck disable=SC1090
-  source $HOME/.zshrc
-  cd . || return 1
-}
-alias reload!='__reload_dotfiles'
-alias souz='source $HOME/.zshrc'
+alias reload!='exec "$SHELL" -l'
 
 # shell nice-to-haves
 alias rmf='rm -rf'
@@ -21,7 +13,6 @@ alias wo='workon'
 # Elixir
 alias iem='iex -S mix'
 alias iemp='iex -S mix phx.server'
-# alias iemp='iex -S mix phoenix.server'
 alias mt='mix test'
 alias mtt='mix test --trace'
 alias mto='mix test --only focus'
